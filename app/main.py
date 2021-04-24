8128fe91f97fc31294248e4ba3ad2c83ea371946
aburmagin <andrey.burmagin.1998@gmail.com>
2021-04-24 10:43:43 +0300

    added mock lots number generating

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    free_lots = random.randint(30, 50)
+    taken_lots = random.randint(20, 40)
+    total_lots = free_lots + taken_lots
+    return jsonify(
+        total=total_lots,
+        free=free_lots,
+        taken=taken_lots
+    )
