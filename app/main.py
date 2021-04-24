8b346b6839e2e8fba3f2a9b31c5eca5f388f7fad
aburmagin <andrey.burmagin.1998@gmail.com>
2021-04-24 21:33:59 +0300

    implemented file receive

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
 import random
 
 app = Flask(__name__)
@@ -14,3 +15,12 @@ def mock():
         free=free_lots,
         taken=taken_lots
     )
+
+
+@app.route('/mask_image', methods=['POST'])
+def mask_image():
+    try:
+        file = request.files['file']
+        return secure_filename(file.filename)
+    except Exception as e:
+        print(str(e))
